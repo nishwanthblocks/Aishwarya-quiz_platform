a6e620ab2e93aaa6f2416d6df721b32b34e5ff6d
nishwanthblocks <148674301+nishwanthblocks@users.noreply.github.com>
2024-07-27 15:45:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const resultDiv = document.getElementById('result');
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+
+    
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is the largest planet in our solar system?',
+    answers: [
+      { text: 'Jupiter', correct: true },
+      { text: 'Saturn', correct: false },
+      { text: 'Uranus', correct: false},
+      {text: 'Earth', correct: false}
+    ]
+  },
+  {
+    question: 'What is the chemical symbol for water?',
+    answers: [
+      { text: 'H20', correct: true },
+      { text: 'H2SO4', correct: false },
+      { text: 'CO2', correct: false },
+      { text: 'None of the above', correct: false }
+    ]
+  },
+  {
+    question: 'Which ocean is the largest and deepest on Earth?',
+    answers: [
+      { text: 'Pacific Ocean', correct: true },
+      { text: 'Arctic Ocean', correct: false},
+      { text: 'Indian Ocean', correct: false },
+      { text: 'Southern Ocean', correct: false }
+    ]
+  },
+  {
+    question: 'What is the hardest natural substance on Earth?',
+    answers: [
+      { text: 'Diamond', correct: true },
+      { text: 'Gold', correct: false },
+      { text: 'Silver', correct: false },
+      { text: 'Iron', correct: false }
+    ]
+  }
+]
